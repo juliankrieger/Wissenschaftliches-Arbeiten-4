2649c165248583f62fd8ad0d27860aa6a6963e13
JKrieger <jkrieger@htwsaar.de>
2019-07-05 12:20:57 +0200

    static content

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import bigjpg from "./bigjpg.jpg";
-import biggif from "./giphy.gif";
-import bigpng from "./hugepng.jpg"
-import './page.css'
 
-document.getElementById("JPG").src = bigjpg;
-document.getElementById("GIF").src = biggif;
-document.getElementById("PNG").src = bigpng;
+
+document.getElementById("JPG").src = './bigjpg.jpg';
+document.getElementById("GIF").src = './giphy.gif';
+document.getElementById("PNG").src = './bigpng.png';
